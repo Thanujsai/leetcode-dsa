806ec505aa763cd171443702b1866e4ae0998bda
thanujsai <sboda@modeln.com>
2025-09-21 14:15:03 +0530

    big O

--- src/datastructures/Main.java
+++ src/datastructures/Main.java
@@ -1,7 +1,16 @@
 package datastructures;
 public class Main {
 
+  public static void printItems(int n){
+    for(int i=0; i<n; i++){//O(n) time complexity
+      System.out.println(i);
+    }
+    
+    for(int i=0; i<n; i++){//O(n) time complexity
+      System.out.println(i);
+    }
+  }
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    printItems(10);
   }
 }
