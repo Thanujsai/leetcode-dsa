6cb3f061f284363f38607b2cf38fd8dc2a1c96be
thanujsai <sboda@modeln.com>
2025-09-21 14:22:34 +0530

    O(n^2)

--- src/datastructures/Main.java
+++ src/datastructures/Main.java
@@ -2,12 +2,10 @@ package datastructures;
 public class Main {
 
   public static void printItems(int n){
-    for(int i=0; i<n; i++){//O(n) time complexity
-      System.out.println(i);
-    }
-    
-    for(int i=0; i<n; i++){//O(n) time complexity
-      System.out.println(i);
+    for(int i=0; i<n; i++){//O(n^2) time complexity
+      for(int j=0; j<n; j++){
+        System.out.println(i + " " + j);
+      }
     }
   }
   public static void main(String[] args) {
