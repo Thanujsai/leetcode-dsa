87e26ffc7241b6571d05ffa1811e66e2cdce0b1d
thanujsai <sboda@modeln.com>
2025-09-24 21:49:37 +0530

    printlist

--- src/datastructures/LinkedList/LinkedList.java
+++ src/datastructures/LinkedList/LinkedList.java
@@ -23,6 +23,26 @@ public class LinkedList {
         length = 1;
     }
 
+    public void printList(){
+        Node temp = head;
+        while(temp != null){
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    public void getHead() {
+        System.out.println("Head: " + head.value);
+    }
+
+    public void getTail() {
+        System.out.println("Tail: " + tail.value);
+    }
+
+    public void getLength() {
+        System.out.println("Length: " + length);
+    }
+
     public void append(int value) {
         //creates a new node
         //adds a new node to the end of the list
--- src/datastructures/Main.java
+++ src/datastructures/Main.java
@@ -4,5 +4,11 @@ import datastructures.LinkedList.LinkedList;
 public class Main {
   public static void main(String[] args) {
     LinkedList myLinkedList = new LinkedList(4);
+
+    myLinkedList.getHead();
+    myLinkedList.getTail();
+    myLinkedList.getLength();
+
+    myLinkedList.printList();
   }
 }
