595dfd4991eb715f12eb821a70787a4033a8d66e
thanujsai <sboda@modeln.com>
2025-09-24 21:40:07 +0530

    create a linked list

--- src/datastructures/Main.java
+++ src/datastructures/Main.java
@@ -1,28 +1,8 @@
 package datastructures;
+import datastructures.LinkedList.LinkedList;
 
 public class Main {
-
-  public static void printItems(int n){
-    for(int i=0; i<n; i++){//O(n^2) time complexity
-      for(int j=0; j<n; j++){
-        System.out.println(i + " " + j);
-      }
-    }
-
-    for(int j=0; j<n; j++){//O(n) time complexity
-      System.out.println(j);
-    }
-
-    //therefore this function has O(n^2 + n) = O(n^2) time complexity since n^2 grows faster than n as n gets larger, n^2 is dominant term
-    //this rules of simplification is called drop non dominant terms
-  }
   public static void main(String[] args) {
-    //printItems(10);
-
-    Add add = new Add();
-    System.out.println(add.add(5, 10));
-
-    Test test = new Test();
-    test.printItems(10, 5);
+    LinkedList myLinkedList = new LinkedList(4);
   }
 }
