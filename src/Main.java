94fb625e82d7b050be2265f819c858eff98b465e
thanujsai <sboda@modeln.com>
2025-10-19 20:17:53 +0530

    hash table constructor, hash method and set method

--- src/Main.java
+++ src/Main.java
@@ -5,24 +5,19 @@ import datastructures.Queue;
 import datastructures.BinarySearchTree;
 import datastructures.DoublyLinkedList;
 import datastructures.DoublyLinkedList.Node;
+import datastructures.HashTable;
 
 public class Main {
   public static void main(String[] args) {
-    BinarySearchTree bst = new BinarySearchTree();
-    System.out.println("Binary Search Tree created with root value: " + bst.root);
+    HashTable ht = new HashTable();
+    
+    ht.set("nails", 100);
+    ht.set("tile", 50);
+    ht.set("lumber", 80);
+    ht.set("bolts", 200);
+    ht.set("screws", 140);
 
-    bst.insert(47);
-    bst.insert(21);
-    bst.insert(76);
-    bst.insert(18);
-    bst.insert(52);
-    bst.insert(82);
-
-    bst.insert(27);
-    System.out.println("expected location of 27 : bst.root.left.right = " + bst.root.left.right.value);
-
-    System.out.println("BST contains 27: " + bst.contains(27));
-    System.out.println("BST contains 17: " + bst.contains(17));
+    ht.printTable();
   }
 
 }
---
+++ src/datastructures/HashTable.java
@@ -0,0 +1,56 @@
+package datastructures;
+
+public class HashTable {
+    private int size = 7;
+    private Node[] dataMap;//array that contains pointers to nodes
+
+    public HashTable() {
+        dataMap = new Node[size];
+    }
+
+    public class Node {
+        String key;
+        int value;
+        Node next;
+
+        public Node(String key, int value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    public void printTable() {
+        for (int i = 0; i < dataMap.length; i++) {
+            System.out.println(i + ":");
+            Node temp = dataMap[i];
+            while (temp != null) {
+                System.out.println("  {" + temp.key + ": " + temp.value + "}");
+                temp = temp.next;
+            }
+        }
+    }
+
+    private int hash(String key) {
+        int hash = 0;
+        char[] keyChars = key.toCharArray();
+        for(int i=0; i<keyChars.length; i++) {
+            int asciiValue = keyChars[i];
+            hash = (hash + asciiValue * 23) % dataMap.length;//using a prime number 23 so that the output is more unique, modulo length of dataMap because we want it to fit in the array
+        }
+        return hash;//this will always be the number between 0 and length-1
+    }
+
+    public void set(String key, int value) {
+        int index = hash(key);
+        Node newNode = new Node(key, value);
+        if(dataMap[index] == null) {
+            dataMap[index] = newNode;
+        } else {
+            Node temp = dataMap[index];
+            while(temp.next != null) {//takes us to the last node in the chain
+                temp = temp.next;
+            }
+            temp.next = newNode;
+        }
+    }
+}
